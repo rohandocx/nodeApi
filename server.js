5f241cc5b8608d59129cbb84c932086415dac204
Your Name <rohan2056.be20@chitkara.edu.in>
2023-05-28 09:25:02 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Node API');
+})
+
+
+
+app.listen(3000,()=>{
+    console.log('listening on port 3000');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
